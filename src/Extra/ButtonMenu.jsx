8d85446690fc4340596f1c0e64a3ba51c32d25db
vinaykumar7580 <vinayhatwar16@gmail.com>
2023-02-07 16:25:44 +0530

    file added

--- src/Extra/ButtonMenu.jsx
+++ src/Extra/ButtonMenu.jsx
@@ -11,6 +11,8 @@ import {
     IconButton
   } from '@chakra-ui/react'
   import {HamburgerIcon,EditIcon} from "@chakra-ui/icons"
+  import * as Scroll from 'react-scroll';
+  let Link=Scroll.Link
 
 function ButtonMenu(){
     return(
@@ -20,25 +22,36 @@ function ButtonMenu(){
     as={IconButton}
     aria-label='Options'
     icon={<HamburgerIcon />}
+    color={'black'}
     variant='outline'
   />
   <MenuList>
-   <a href="#home"><MenuItem icon={<EditIcon/>}>
+    <Link activeClass="nav_link" to="home" spy={true} smooth={true} offset={50} duration={500}>
+    <MenuItem icon={<EditIcon/>} color={'black'}>
     <h3>Home</h3>
-    </MenuItem></a>
-    <a href="#about"><MenuItem icon={<EditIcon/>}>
+    </MenuItem>
+    </Link>
+    <Link activeClass="nav_link" to="about" spy={true} smooth={true} offset={50} duration={500}>
+    <MenuItem icon={<EditIcon/>} color={'black'}>
     <h3>About</h3>
-    </MenuItem></a>
-    <a href="#skills"><MenuItem icon={<EditIcon/>}>
+    </MenuItem>
+    </Link>
+    <Link activeClass="nav_link" to="skills" spy={true} smooth={true} offset={50} duration={500}>
+    <MenuItem icon={<EditIcon/>} color={'black'}>
     <h3>Skills</h3>
-    </MenuItem></a>
-    <a href="#projects"><MenuItem icon={<EditIcon/>}>
+    </MenuItem>
+    </Link>
+    <Link activeClass="nav_link" to="projects" spy={true} smooth={true} offset={50} duration={500}>
+    <MenuItem icon={<EditIcon/>} color={'black'}>
     <h3>Projects</h3>
-    </MenuItem></a>
-    <a href="#contact"><MenuItem icon={<EditIcon/>}>
+    </MenuItem>
+    </Link>
+    <Link activeClass="nav_link" to="contact" spy={true} smooth={true} offset={50} duration={500}>
+    <MenuItem icon={<EditIcon/>} color={'black'}>
     <h3>Contact</h3>
-    </MenuItem></a>
-    <a href="#"><MenuItem icon={<EditIcon/>}>
+    </MenuItem>
+    </Link>
+    <a href="Vinaykumar_Hatwar_Resume..pdf" download><MenuItem icon={<EditIcon/>} color={'black'}>
     <h3>Resume</h3>
     </MenuItem></a>
   </MenuList>
