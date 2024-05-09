4779b92f600c88d5f05dbd8b5b609ba3d49eaea7
vinaykumar7580 <vinayhatwar16@gmail.com>
2024-05-09 15:33:29 +0530

    update image

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -29,7 +29,7 @@ function About() {
           <div className={style.about_second}>
             <img
               className={style.about_second_img}
-              src="https://camo.githubusercontent.com/c1dcb74cc1c1835b1d716f5051499a2814c683c806b15f04b0eba492863703e9/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f3733303730332f73637265656e73686f74732f363538313234332f6176656e746f2e676966"
+              src="https://img.freepik.com/premium-vector/ceo-chief-executive-officer-cartoon-illustration-businessman-work_2175-4824.jpg?size=626&ext=jpg&ga=GA1.1.1056998353.1659457435&semt=ais"
               alt="laptop"
             />
           </div>
